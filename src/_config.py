2d92c0b77761139b360671476e705998d0df65cc
Max Shen <maxwshen@gmail.com>
2016-06-23 09:45:20 -0400

    first

---
+++ src/_config.py
@@ -0,0 +1,22 @@
+PRJ_DIR = '/cluster/mshen/prj/.../'  
+SRC_DIR = PRJ_DIR + 'src/'
+
+toy = True
+# toy = False
+if toy:
+  PRJ_DIR += 'toy/'
+#######################################################
+# Note: Directories should end in / always
+#######################################################
+DATA_DIR = PRJ_DIR + 'data/'
+OUT_PLACE = PRJ_DIR + 'out/'
+RESULTS_PLACE = PRJ_DIR + 'results/'
+#######################################################
+#######################################################
+
+CLEAN = False       # Values = 'ask', True, False
+
+#######################################################
+# Project-specific parameters
+#######################################################
+
